c31f6904bacd768e8aabca59dc15161c97f3eeca
Daniel Cordova <javierdanielcordova@gmail.com>
2023-06-20 20:13:40 +0200

    avance 1

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,18 @@
 import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
 
 const Home = () => {
-  console.log(supabase);
+  const [fetchError, setfetchError] = useState(null)
+  const [smoothies, setSmoothies] = useState(null)
+
+  useEffect(() => {
+    const fetchSmoothies = async () =>{
+      const { data, error } = await supabase
+      .from('smoothies')
+      .select()
+    }
+  }, [])
+
   return (
     <div className="page home">
       <h2>Home</h2>
